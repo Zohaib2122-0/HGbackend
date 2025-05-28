916441ede8175de5fc02468dbe1207fb7c2c7618
zohaib-khalid770 <zohaib.ch.2208@gmail.com>
2025-05-29 00:27:50 +0500

    change index.js

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 
+// import { dbarea } from "./config/dbarea.js";
+// import cors from "cors"
+
+// import { app } from "./api/controller/patientcontroller.js";
+// import billingroutes from "./api/routes/billingroutes.js";
+// import patientroutes from "./api/routes/patientroutes.js";
+// import { userroutes } from "./api/routes/userroutes.js";
+// import doctorroutes from "./api/routes/doctoreroutes.js";
+// import treatmentroutes from "./api/routes/treatmentroutes.js";
+// import messagesroutes from "./api/routes/messagesroutes.js";
+// import cookieParser from "cookie-parser";
+// let hm = () => {
+//     app.use(cookieParser())
+//     app.use(cors({
+
+//         origin: 'http://localhost:5173',
+//         credentials: true
+//     }
+//     ))
+//     app.options("*", cors());
+//     let port = 3000
+
+//     app.listen(port, () => {
+//         app.use("/api", patientroutes)
+//         app.use("/api", billingroutes)
+//         app.use("/api", userroutes)
+//         app.use("/api", doctorroutes)
+//         app.use("/api", treatmentroutes)
+//         app.use("/api", messagesroutes)
+//         dbarea()
+//         console.log(`hm server started sucessfully on port${port}`)
+//     })
+
+// }
+// hm()
+
+
+
 import { dbarea } from "./config/dbarea.js";
 import cors from "cors"
-
-import { app } from "./api/controller/patientcontroller.js";
+import express from "express";
 import billingroutes from "./api/routes/billingroutes.js";
 import patientroutes from "./api/routes/patientroutes.js";
 import { userroutes } from "./api/routes/userroutes.js";
@@ -10,27 +47,24 @@ import doctorroutes from "./api/routes/doctoreroutes.js";
 import treatmentroutes from "./api/routes/treatmentroutes.js";
 import messagesroutes from "./api/routes/messagesroutes.js";
 import cookieParser from "cookie-parser";
-let hm = () => {
-    app.use(cookieParser())
-    app.use(cors({
 
-        origin: 'http://localhost:5173',
-        credentials: true
-    }
-    ))
-    app.options("*", cors());
-    let port = 3000
+// Initialize express app
+const app = express();
 
-    app.listen(port, () => {
-        app.use("/api", patientroutes)
-        app.use("/api", billingroutes)
-        app.use("/api", userroutes)
-        app.use("/api", doctorroutes)
-        app.use("/api", treatmentroutes)
-        app.use("/api", messagesroutes)
-        dbarea()
-        console.log(`hm server started sucessfully on port${port}`)
-    })
+app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
+app.options("*", cors());
 
-}
-hm()
+app.use("/api", patientroutes);
+app.use("/api", billingroutes);
+app.use("/api", userroutes);
+app.use("/api", doctorroutes);
+app.use("/api", treatmentroutes);
+app.use("/api", messagesroutes);
+
+dbarea();
+
+export default app;
