f6dce521fe9dc95ed73d8e29539c5915d7327ca1
zohaib-khalid770 <zohaib.ch.2208@gmail.com>
2025-06-02 16:19:59 +0500

    add some

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(cors({
     // origin: 'https://faisal-hm-frontend.vercel.app',
 
-origin: ["http://localhost:5173", "https://faisal-hm.vercel.app"],
+origin: ["http://localhost:5173", "https://hammadportfolio-ochre.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true
 
